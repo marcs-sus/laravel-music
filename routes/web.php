960e75395321bceae0fb8f63ddec8eab6f66d334
marcs-sus <marcosviniciuscunha@proton.me>
2026-05-16 18:04:48 -0300

    Update web routes (Breeze)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\PlaylistController;
+use App\Http\Controllers\SongController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +20,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('albums', AlbumController::class);
+    Route::resource('artists', ArtistController::class);
+    Route::resource('genres', GenreController::class);
+    Route::resource('playlists', PlaylistController::class);
+    Route::resource('songs', SongController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
